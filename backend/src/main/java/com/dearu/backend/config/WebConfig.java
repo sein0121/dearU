a31a4331b15a80c1b3f99ede9c2d38b38d55e616
sein0121 <sein0121@gmail.com>
2025-03-05 19:03:56 +0900

    0305 : 로그인, 회원가입 기능 수정

--- backend/src/main/java/com/dearu/backend/config/WebConfig.java
+++ backend/src/main/java/com/dearu/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")  // React URL
+                        .allowedOrigins("http://localhost:3000")  // React URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- backend/src/main/java/com/dearu/backend/controller/UserController.java
+++ backend/src/main/java/com/dearu/backend/controller/UserController.java
@@ -6,7 +6,7 @@ import com.dearu.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
